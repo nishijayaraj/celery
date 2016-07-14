60db9b79c317a5dba03f2c60d9d591690bd6c4f4
SPACE <space@space-Vostro-3800.(none)>
2016-07-14 12:45:52 +0530

    tested code with rest api using curl

---
+++ README.md
@@ -0,0 +1,12 @@
+This project is for showing how celery http-gateway can be used for REST api calls. For this we need to run this project in a virtual environment .And install celery django and all required modules.
+
+Running :
+
+Once everything is installed, This project can be run by executing following commands:
+
+python manage.py runserver
+
+
+And the app this djnago app would start to listen to the port 8080 and to which we can send rest api calls
+
+
---
+++ tasks.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, unicode_literals
+
+from celery import Celery,task
+from time import sleep
+
+app = Celery('tasks')
+app.config_from_object('celeryconfig')
+
+@app.task() 
+def hello_world():
+    return 'Hello world.........'
+
+@app.task()
+def UploadTask():
+ 
+    # Update the state. The meta data is available in task.info dicttionary
+    # The meta data is useful to store relevant information to the task
+    # Here we are storing the upload progress in the meta. 
+ 
+    UploadTask.update_state(state='PROGRESS', meta={'progress': 0})
+    sleep(30)
+    UploadTask.update_state(state='PROGRESS', meta={'progress': 30})
+    sleep(30)
+    
